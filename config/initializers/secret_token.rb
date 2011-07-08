7d9e4d6a837ea00049ba4e62fd30d37353a0520d
Graeme Mathieson <mathie@woss.name>
2011-07-08 21:24:00 +0100

    rails new t_tables -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TTables::Application.config.secret_token = '494eae4ccc3a240cb5737d5c888a2456b24cd521e7a574c22fa8c655b98ff115870a52f971f450071d047659ce1de7fbd0a5e3ae861b685eb5b69b5c6c80b34f'
