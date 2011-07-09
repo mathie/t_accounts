b8ce0b4ca6f3ca92c512f3d799de5adcb9ff91b9
Graeme Mathieson <mathie@woss.name>
2011-07-09 15:39:37 +0100

    Allow the worksheet to be edited.
    Plus add flash messages for good measure.

--- app/controllers/worksheets_controller.rb
+++ app/controllers/worksheets_controller.rb
@@ -16,9 +16,24 @@ class WorksheetsController < ApplicationController
     respond_with(@worksheet)
   end
 
+  def edit
+    @worksheet = Worksheet.find(params[:id])
+    respond_with(@worksheet)
+  end
+
   def create
     @worksheet = Worksheet.new(params[:worksheet])
-    @worksheet.save
+    if @worksheet.save
+      flash[:notice] = "Worksheet created."
+    end
+    respond_with(@worksheet)
+  end
+
+  def update
+    @worksheet = Worksheet.find(params[:id])
+    if @worksheet.update_attributes(params[:worksheet])
+      flash[:notice] = "Worksheet details successfully updated."
+    end
     respond_with(@worksheet)
   end
 end
