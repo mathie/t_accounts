c3db1a3f3de60abff4db64a7ab38f6c4e6af684c
Graeme Mathieson <mathie@woss.name>
2011-07-09 22:23:48 +0100

    Wire up the T Accounts.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,7 @@
 class Account < ActiveRecord::Base
   belongs_to :worksheet
+  has_many :credit_transactions, class_name: 'Transaction', foreign_key: 'credit_account_id'
+  has_many :debit_transactions,  class_name: 'Transaction', foreign_key: 'debit_account_id'
 
   default_scope :order => :code
 
@@ -17,5 +19,49 @@ class Account < ActiveRecord::Base
       # Let the validations deal with invalid codes.
       write_attribute(:code, new_code)
     end
+  end
+
+  def zipped_transactions
+    debit_transactions.zip(credit_transactions)
+  end
+
+  def debits_subtotal
+    debit_transactions.sum :amount
+  end
+
+  def credits_subtotal
+    credit_transactions.sum :amount
+  end
+
+  def balance_carried_down?
+    debit_balance_carried_down? || credit_balance_carried_down?
+  end
+
+  def debit_balance_carried_down?
+    debits_subtotal < credits_subtotal
+  end
+
+  def credit_balance_carried_down?
+    debits_subtotal > credits_subtotal
+  end
+
+  def balance_carried_down
+    (credits_subtotal - debits_subtotal).abs
+  end
+
+  def debit_balance_brought_down?
+    debits_subtotal > credits_subtotal
+  end
+
+  def credit_balance_brought_down?
+    debits_subtotal < credits_subtotal
+  end
+
+  def balance_brought_down
+    balance_carried_down
+  end
+
+  def total
+    [credits_subtotal, debits_subtotal].max
   end
 end
