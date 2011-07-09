23a8570c45543946d2292d751a9003f49d8f7e6b
Graeme Mathieson <mathie@woss.name>
2011-07-09 16:41:22 +0100

    Allow accounts to be edited, too.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -7,10 +7,22 @@ class AccountsController < ApplicationController
     @account = @worksheet.accounts.build
   end
 
+  def edit
+    @account = @worksheet.accounts.find(params[:id])
+  end
+
   def create
     @account = @worksheet.accounts.build(params[:account])
     if @account.save
       flash[:notice] = "Account #{@account.name} created."
+    end
+    respond_with(@worksheet, @account, location: @worksheet)
+  end
+
+  def update
+    @account = @worksheet.accounts.find(params[:id])
+    if @account.update_attributes(params[:account])
+      flash[:notice] = "Account #{@account.name} updated."
     end
     respond_with(@worksheet, @account, location: @worksheet)
   end
