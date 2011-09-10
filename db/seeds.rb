76e6470be83ffef54ac3c389bea2f9b66d5b353e
Graeme Mathieson <mathie@woss.name>
2011-09-10 15:44:54 +0100

    Create a user with some random worksheets.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create a default user so I don't have to keep signing up...
+unless User.exists?(email: 'mathie@woss.name')
+  user = User.new email: 'mathie@woss.name',
+    password:              'password',
+    password_confirmation: 'password',
+    name:                  'Graeme Mathieson'
+  user.confirmed_at = Time.now
+  user.save!
+
+  20.times do
+    user.worksheets.create! name: Faker::Company.bs, narrative: Faker::Lorem.paragraphs.join("\n\n")
+  end
+end
