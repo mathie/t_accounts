5acfe8997f7fed615c17e5ceb83024317e1501f8
Graeme Mathieson <mathie@woss.name>
2011-09-10 16:44:52 +0100

    Seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,22 @@ unless User.exists?(email: 'mathie@woss.name')
   user.save!
 
   20.times do
-    user.worksheets.create! name: Faker::Company.bs, narrative: Faker::Lorem.paragraphs.join("\n\n")
+    worksheet = user.worksheets.create! name: Faker::Company.bs, narrative: Faker::Lorem.paragraphs.join("\n\n")
+
+    # Create a few accounts
+    accounts = []
+    rand(10).times do
+      accounts << worksheet.accounts.create!(name: Faker::Company.bs)
+    end
+
+    # Create a bunch of transactions
+    date_range = ((Date.today - 1.year)..Date.today).to_a
+    rand(100).times do
+      worksheet.transactions.create! dated_on: date_range.sample,
+        description:       Faker::Company.bs,
+        debit_account_id:  accounts.sample.id,
+        credit_account_id: accounts.sample.id,
+        amount:            BigDecimal(rand(10_000).to_s) / BigDecimal('100')
+    end
   end
 end
